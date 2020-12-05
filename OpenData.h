633711a55ffc8d01b51c276ac2245cfa3713bf09
dev-iks <bondarchuk.nik.nb@gmail.com>
2020-12-06 00:03:41 +0200

    Remove unnecessary variables

--- OpenData.h
+++ OpenData.h
@@ -12,17 +12,13 @@ typedef struct OW_current {
   uint8_t  humidity = 0;
   uint8_t  clouds = 0;
   uint32_t visibility = 0;
-  float    wind_speed = 0;
-  float    wind_gust = 0;
+  int    wind_speed = 0;
   uint16_t wind_deg = 0;
-  float    rain = 0;
-  float    snow = 0;
 
   // current.weather
   uint16_t id = 0;
   String   city;
   String   main;
-  String   params;
   String   description;
 
 } OW_current;
